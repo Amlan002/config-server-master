67c181da29cf266dfddbc5cfe7ca71723accd790
amlan2 <50303264+amlan2@users.noreply.github.com>
2019-05-14 00:34:44 +0530

    khh

--- src/main/java/com/amlan/ConfigServerMasterApplication.java
+++ src/main/java/com/amlan/ConfigServerMasterApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-@ConfigurationProperties("localconfig")
 public class ConfigServerMasterApplication {
 
 	public static void main(String[] args) {
